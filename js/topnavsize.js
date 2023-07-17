d55c6ce66458a3a8530c1e48804757c713bebf33
Rui-M1 <40121317+Rui-M1@users.noreply.github.com>
2023-07-17 04:22:40 +0200

    there is more tomorrow

--- js/topnavsize.js
+++ js/topnavsize.js
@@ -1,8 +1,12 @@
-function navsize() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+const dropdownIcon = document.querySelector('.dropdown-icon');
+const menuHeader = document.querySelector('.menuheader');
+
+dropdownIcon.addEventListener('click', function () {
+  menuHeader.classList.toggle('show');
+});
+
+window.addEventListener('resize', function () {
+  if (window.innerWidth >= 600) {
+    menuHeader.classList.remove('show');
   }
+});
